fc5c6bec7b2e97f6700c40f509777a6f6261c926
pouriya-zarbafian <pouriya.zarbafian@gmail.com>
2020-02-27 00:41:25 +0100

    read config

---
+++ src/config.rs
@@ -0,0 +1,45 @@
+use std::env;
+use std::env::VarError;
+use std::fs;
+use std::collections::HashMap;
+use std::error::Error;
+
+const ENV_CONFIG_PATH: &str = "DRAFT_CONFIG";
+const PROPERTY_SEPARATOR: &str = "=";
+
+pub fn get_config_file() -> Result<String, VarError>{
+
+    env::var(ENV_CONFIG_PATH)
+}
+
+#[derive(Debug)]
+pub struct Config {
+    members: String,
+    me: String,
+}
+impl Config {
+    pub fn new(path: &String) -> Result<Config, Box<dyn Error>> {
+        let properties = read_config(path)?;
+
+        Ok(Config{
+            members: String::from(properties.get("members").expect("Missing property: members")),
+            me: String::from(properties.get("me").expect("Missing property: me")),
+        })
+    }
+}
+
+fn read_config(path: &String) -> Result<HashMap<String, String>, Box<dyn Error>> {
+
+    let contents = fs::read_to_string(path)?;
+
+    let mut map = HashMap::new();
+
+    for line in contents.lines().filter(|line| { !line.starts_with("#") && !line.trim().is_empty()}) {
+
+        let eq_pos = line.find(PROPERTY_SEPARATOR).expect(format!("Invalid line in property file: {}", line).as_str());
+        let (key, value) = line.split_at(eq_pos);
+        map.insert(String::from(key), String::from(&value[1..]));
+    }
+
+    Ok(map)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::process;
+
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    // read configuration path
+    let config_file = config::get_config_file().unwrap_or_else(|err| {
+        eprintln!("Error reading environment variable: {}", err);
+        process::exit(1)
+    });
+    // parse configuration file
+    let config = config::Config::new(&config_file).unwrap_or_else(|err| {
+        println!("Error parsing configuration: {}", err);
+        process::exit(1)
+    });
+    println!("{:?}", config);
 }
